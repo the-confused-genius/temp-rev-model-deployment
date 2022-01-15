bada5f4a982df011a3bc334ca61d796b6598b061
Mohammed Adnan Khan <90715773+the-confused-genius@users.noreply.github.com>
2022-01-15 14:53:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#from distutils.log import debug
+import pickle
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/pred', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == "POST":
+        prediction = model.predict([[int(request.form.get('temperature'))]])
+        output = round(prediction[0], 2)
+        return render_template('index.html', prediction_text=f'The Predicted Revenue is Rs. {output}/-')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
