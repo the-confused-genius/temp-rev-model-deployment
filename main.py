f6ec29777f27eb351c454fe38581e789a3dd309c
Mohammed Adnan Khan <90715773+the-confused-genius@users.noreply.github.com>
2022-01-15 17:37:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
+
 @app.route('/', methods=['GET', 'POST'])
 def predict():
     if request.method == "POST":
-        prediction = model.predict([[int(request.form.get('temperature'))]])
-        output = round(prediction[0], 2)
-        return render_template('index.html', prediction_text=f'The Predicted Revenue is Rs. {output}/-')
+        try:
+            x = int(request.form.get('temperature'))
+            output = round(model.predict([[x]])[0], 2)
+            return render_template('index.html', prediction_text=f'The Predicted Revenue is Rs. {output}/-')
+        except:
+            output = "Invalid Input !"
+            return render_template('index.html', prediction_text=output)
     else:
         return render_template('index.html')
 
