b9622332f27b3a60c7bfa78417b189553c8140d2
Mohammed Adnan Khan <90715773+the-confused-genius@users.noreply.github.com>
2022-01-15 15:55:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
-
-
-@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == "POST":
         prediction = model.predict([[int(request.form.get('temperature'))]])
         output = round(prediction[0], 2)
-        return render_template('templates\index.html', prediction_text=f'The Predicted Revenue is Rs. {output}/-')
+        return render_template('index.html', prediction_text=f'The Predicted Revenue is Rs. {output}/-')
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
